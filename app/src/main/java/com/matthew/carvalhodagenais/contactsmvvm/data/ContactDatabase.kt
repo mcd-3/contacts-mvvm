951c292caf6f6c9dee33a9799d3d0cbcde6d70a1
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2019-12-14 00:30:42 -0500

    Removed auto-inserted db entry

--- app/src/main/java/com/matthew/carvalhodagenais/contactsmvvm/data/ContactDatabase.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/contactsmvvm/data/ContactDatabase.kt
@@ -1,15 +1,13 @@
 package com.matthew.carvalhodagenais.contactsmvvm.data
 
 import android.content.Context
-import android.os.AsyncTask
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.matthew.carvalhodagenais.contactsmvvm.data.dao.ContactDao
 import com.matthew.carvalhodagenais.contactsmvvm.data.entities.Contact
 
-@Database(entities = [Contact::class], version = 2)
+@Database(entities = [Contact::class], version = 3)
 abstract class ContactDatabase: RoomDatabase() {
 
     abstract fun contactDao(): ContactDao
@@ -26,27 +24,10 @@ abstract class ContactDatabase: RoomDatabase() {
                         ContactDatabase::class.java,
                         "contact_database"
                     ).fallbackToDestructiveMigration()
-                        .addCallback(roomCallBack)
                         .build()
                 }
             }
             return instance
-        }
-
-        private val roomCallBack = object: RoomDatabase.Callback() {
-            override fun onCreate(db: SupportSQLiteDatabase) {
-                super.onCreate(db)
-                PopulateDBAsyncTask(instance).execute()
-            }
-        }
-
-
-        private class PopulateDBAsyncTask(database: ContactDatabase?): AsyncTask<Unit, Unit, Unit>() {
-            private val dao = database?.contactDao()
-            override fun doInBackground(vararg params: Unit?) {
-                dao?.insert(Contact("Matt", "C-D", "mcd@email.com",
-                    "5145551111", "", "", "111 Example Street", null, null))
-            }
         }
     }
 }
